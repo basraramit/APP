ca888d73f2297f924af87cc7686b0fbfd64cb30b
basraramit <32370997+basraramit@users.noreply.github.com>
2017-10-07 10:15:56 -0400

    Add files via upload

---
+++ RoundRobin.java
@@ -0,0 +1,67 @@
+package com.Risk;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RoundRobin{
+	private ArrayList<> players;
+	private ArrayList<Country> countries;
+	CountryMap map;
+	private Player curPlayer=new Player();
+	private int gameState;
+	
+	public void RoundRobin(ArrayList<Player> PlayerList){
+
+		players=PlayerList;
+		countries=map.getCountryList();
+
+		
+		initializePlayer();
+	}
+
+	public void initializePlayer(){
+		curPlayer=players.get(0);
+	}
+
+	getCurrentPlayer(){
+		return curPlayer;
+	}
+	public ArrayList<Player> getPlayers(){
+        return players;
+    }
+    public int getState(){
+        return gameState;
+    }
+
+    public void setState(int state){
+        gameState = state;
+    }
+
+	void placeArmy(int CountryID){
+		for(int i=0;i<countries.size();i++){
+			Country temp=countries.get(i);
+			if(temp.getCountryId()==CountryID && temp.getPlayerName()==curPlayer.GetplayerName()){
+				if(curPlayer.getArmy()>0){
+					temp.addArmies(1);
+					curPlayer.setArmy(curPlayer.getArmy()--);		
+				}else{
+					System.out.println("You have no army left");
+
+				}
+				
+			}else{
+					System.out.println("This is not you territory");
+					return;
+			}
+		}
+		 if(players.indexOf(curPlayer)<players.size()){
+                curPlayer=players.get(players.indexOf(curPlayer)+1);
+            }else{
+               // setState(ACTIVE_TURN);
+            	//START NEXT PHASE (NOT IMPLEMENTED YET)
+         }
+
+	}
+}
