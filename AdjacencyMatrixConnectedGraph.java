187b15e3f5fd33dad462091d61409a68f7392285
urbashi3191 <32408056+urbashi3191@users.noreply.github.com>
2017-10-19 06:17:20 -0230

    Update AdjacencyMatrixConnectedGraph.java

--- AdjacencyMatrixConnectedGraph.java
+++ AdjacencyMatrixConnectedGraph.java
@@ -8,35 +8,37 @@ import java.util.List;
 import java.util.Scanner;
 import Map.CountryMap;
 
+/** This class takes the map file
+* to check the connectivity among countries
+* by adjacency matrix
+*/
 public class AdjacencyMatrixConnectedGraph 
 {            public boolean checkAdjacency(){
     	    	String fileName= "MatrixFile.txt";
     	        int lineNo = 0;
     	        File file= new File(fileName);
-
-    	        // this gives you a 2-dimensional array of strings
-    	        List<List<String>> lines = new ArrayList<>();    
+                List<List<String>> lines = new ArrayList<>();    
     	        List<List<String>> adjcencyMatrix = new ArrayList<>();         
     	        Scanner inputStream;
     	        
     	        try{
     	            inputStream = new Scanner(file);
 
-    	            while(inputStream.hasNext()){
-    	                String line= inputStream.next();
-    	                if (line.equals("[Territories]")){
-    	                   inputStream.nextLine();
-    	                   lineNo = 0;
-    	                   while (inputStream.hasNextLine()){       	                		
-    	  	              line = inputStream.nextLine();
-    	                   String[] values = line.split(",");             
-    	                   // this adds the currently parsed line to the 2-dimensional string array
-    	                   lines.add(Arrays.asList(values));
-    	                  //System.out.println(line);
-    	                  lineNo++;
-    	            }
-    	                	   
+    	            while(inputStream.hasNext())
+                    {
+    	                     String line= inputStream.next();
+    	                              if (line.equals("[Territories]")){
+    	                                      inputStream.nextLine();
+    	                                      lineNo = 0;
+    	                while (inputStream.hasNextLine())
+                        {       	                		
+    	  	                  line = inputStream.nextLine();
+    	                      String[] values = line.split(",");             
+    	                      lines.add(Arrays.asList(values));
+    	                      lineNo++;
     	                }
+    	                	   
+    	              }
     	            }
 
     	            inputStream.close();
@@ -44,29 +46,30 @@ public class AdjacencyMatrixConnectedGraph
     	           e.printStackTrace();
     	        }
     	          	        	                 	           	        
-    	        //Filling the adjacency Matrix    
-    	        //System.out.println(lineNo);  
+    	          
     	        int[][] adjMatrix = new int[lineNo][lineNo];
-    	        for(List<String> line: lines) {    	        	       	
-    	           int id = Integer.valueOf(line.get(0));    	        	            
-    	           for (int i=4; i<line.size(); i++){	    	              
+    	        for(List<String> line: lines)
+                {    	        	       	
+    	                 int id = Integer.valueOf(line.get(0));    	        	            
+    	             for (int i=4; i<line.size(); i++)
+                     {	    	              
     	                adjMatrix[id][Integer.valueOf(line.get(i))] = 1;    	                    	              
-    	            }    	            	           
-    	       }
+    	             }    	            	           
+    	        }
     	        
     	        String mLine = "";
-    	        for (int i = 0; i< lineNo; i++){
-    	            for (int j = 0; j < lineNo ; j++ ){
+    	        for (int i = 0; i< lineNo; i++)
+                {
+    	            for (int j = 0; j < lineNo ; j++ )
+                    {
     	            	if (i == j)
     	            	adjMatrix[i][j] = 0;   	            	    	            	          	   	            	
-    	            	//System.out.print(adjMatrix[i][j] + " "); 
     	            }    	          
-    	           	//System.out.println();
-    	            }   	               	       	           
-        boolean result = isConnected(adjMatrix);
-       
-  
-        if (result)
+    	         }   	               	       	           
+        
+    boolean result = isConnected(adjMatrix);
+      
+    if (result)
         {
             System.out.println("The graph is connected");
         }else
@@ -75,37 +78,40 @@ public class AdjacencyMatrixConnectedGraph
         }
         return result;
      }
+ 
+ /** This method checks connectivity of graph
+   * @param aadjacencyMatrix
+   * @return
+  */
   
     private static boolean isConnected(int[][] adjacencyMatrix) 
     {
-        int visited[] = new int[adjacencyMatrix.length ];
+        int visited[] = new int[adjacencyMatrix.length];
   
-        for(int row=0; row< adjacencyMatrix.length; row++)
-        {
-            for(int col=0; col < adjacencyMatrix.length; col++)
-            {
-            	//System.out.print(adjacencyMatrix[row][col]);
-                if(adjacencyMatrix[row][col]==1 && visited[row]==0)
-                {
-                    visited[row] = 1;
-                }  
-            }
-            //System.out.println();
-        }
+             for(int row=0; row< adjacencyMatrix.length; row++)
+             {
+                  for(int col=0; col < adjacencyMatrix.length; col++)
+                    {
+            	         if(adjacencyMatrix[row][col]==1 && visited[row]==0)
+                         {
+                          visited[row] = 1;
+                          }  
+                     }
+               }
   
         boolean connected = false;
   
         for (int vertex = 0; vertex < adjacencyMatrix.length; vertex++)
-        {
-            if (visited[vertex] == 1) 
-            {
+           {
+               if (visited[vertex] == 1) 
+               {
                 connected = true;
-            } else
-            {
+               } else
+               {
                 connected = false;
                 break;
+                }
             }
-        }
   
         return connected;
   
